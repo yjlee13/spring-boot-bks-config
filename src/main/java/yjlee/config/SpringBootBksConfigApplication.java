4672b490197d0bbce6aaa268fdc8f568cfbafa56
yjlee-PC\yjlee <yongjin48@gmail.com>
2018-10-08 00:05:11 +0900

    스타트

---
+++ src/main/java/yjlee/config/SpringBootBksConfigApplication.java
@@ -0,0 +1,12 @@
+package yjlee.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBksConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBksConfigApplication.class, args);
+	}
+}
---
+++ src/test/java/yjlee/config/SpringBootBksConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package yjlee.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootBksConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
